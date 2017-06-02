3eb59fc84d5c34710a43436e48c5f1d61820dc41
Fedor Indutny <fedor@indutny.com>
2017-06-02 15:34:25 -0400

    initial

---
+++ index.js
@@ -0,0 +1,65 @@
+'use strict';
+
+const LO_FIELD = 0x10000000000000;
+
+function A64(num) {
+  this.hi = 0;
+  this.lo = 0;
+
+  if (num)
+    this.from(num);
+}
+module.exports = A64;
+
+A64.prototype.from = function from(num) {
+  this.hi = (num / 0x100000000) >>> 0;
+  this.lo = num >>> 0;
+};
+
+A64.prototype.clone = function clone() {
+  const res = new A64();
+  res.hi = this.hi >>> 0;
+  res.lo = this.lo >>> 0;
+  return res;
+};
+
+function pad8(s) {
+  var res = s;
+  while (res.length < 8)
+    res = '0' + res;
+  return res;
+}
+
+A64.prototype.toString = function toString() {
+  return pad8(this.hi.toString(16)) + pad8(this.lo.toString(16));
+};
+
+A64.prototype.iadd = function iadd(other) {
+  const lo = (this.lo + other.lo) >>> 0;
+  const carry = (lo < this.lo) | (lo < other.lo);
+
+  this.hi = ((this.hi + other.hi) | 0 + carry) >>> 0;
+  this.lo = lo;
+
+  return this;
+};
+
+A64.prototype.imul = function imul(other) {
+  const hi = (Math.imul(this.hi, other.lo) + Math.imul(other.hi, this.lo)) | 0;
+  const carry = Math.floor((this.lo * other.lo) / 0x100000000) | 0;
+
+  this.hi = (hi + carry) >>> 0;
+  this.lo = Math.imul(this.lo, other.lo) >>> 0;
+
+  return this;
+};
+
+// Convenience
+
+A64.prototype.add = function add(other) {
+  return this.clone().iadd(other);
+};
+
+A64.prototype.mul = function mul(other) {
+  return this.clone().imul(other);
+};
---
+++ test/a64-test.js
@@ -0,0 +1,88 @@
+'use strict';
+
+const assert = require('assert');
+const BN = require('bn.js');
+const A64 = require('../');
+
+function rnd32() {
+  return (Math.random() * 0x10000000) >>> 0;
+}
+
+describe('Awesome64', () => {
+  describe('add', () => {
+    it('should add without carry', () => {
+      const a = new A64(123);
+      const b = new A64(592);
+
+      assert.equal(a.add(b).toString(), '00000000000002cb');
+    });
+
+    it('should add with carry', () => {
+      const a = new A64(0x1fffffff);
+      const b = new A64(0xeffffeda);
+
+      assert.equal(a.add(b).toString(), '000000010ffffed9');
+    });
+
+    it('should add with overflow', () => {
+      const a = new A64(2 ** 63 + 2 ** 62);
+      const b = new A64(2 ** 63 + 2 ** 62);
+
+      assert.equal(a.add(b).toString(), '8000000000000000');
+    });
+
+    it('should cross-check against BN', () => {
+      for (let i = 0; i < 1e4; i++) {
+        const a = new A64();
+        const b = new A64();
+
+        a.hi = rnd32();
+        a.lo = rnd32();
+
+        b.hi = rnd32();
+        b.lo = rnd32();
+
+        const an = new BN(a.toString(16), 16);
+        const bn = new BN(b.toString(16), 16);
+
+        assert.equal(a.add(b).toString(),
+                     an.add(bn).maskn(64).toString(16, 16));
+      }
+    });
+  });
+
+  describe('mul', () => {
+    it('should mul without carry', () => {
+      const a = new A64(123);
+      const b = new A64(592);
+
+      assert.equal(a.mul(b).toString(), '0000000000011c70');
+    });
+
+    it('should mul with overflow', () => {
+      const a = new A64(0x1fffffff);
+      const b = new A64(0xeffffeda);
+
+      assert.equal(a.mul(b).toString(), '1dffffda50000126');
+    });
+
+    it('should cross-check against BN', () => {
+      for (let i = 0; i < 1e4; i++) {
+        const a = new A64();
+        const b = new A64();
+
+        a.hi = rnd32();
+        a.lo = rnd32();
+
+        b.hi = rnd32();
+        b.lo = rnd32();
+
+        const an = new BN(a.toString(16), 16);
+        const bn = new BN(b.toString(16), 16);
+
+        assert.equal(a.mul(b).toString(),
+                     an.mul(bn).maskn(64).toString(16, 16));
+      }
+    });
+  });
+});
